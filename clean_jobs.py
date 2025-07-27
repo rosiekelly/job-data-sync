371cc8d512c599b948aa647beecae217fd527c98
rosiekelly <rosiekelly2000@outlook.com>
2025-07-27 18:13:55 +0100

    Update clean_jobs.py

--- clean_jobs.py
+++ clean_jobs.py
@@ -1,8 +1,4 @@
-import json
-
-# Load all_jobs from a JSON file
-with open("jobs.json", "r", encoding="utf-8") as f:
-    all_jobs = json.load(f)
+dimport json
 
 cleaned_jobs = []
 skipped_jobs = []
